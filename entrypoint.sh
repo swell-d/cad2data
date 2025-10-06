70643305eb97b5f0e98ea80b13d2a8cfbf811e38
Dmitry Rude <3952800@gmail.com>
2025-10-06 16:23:41 +0200

    use command line params

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
+IN_FILE="$1"
+OUT_FILE="$2"
 if [[ "${DEBUG:-0}" == "1" ]]; then export WINEDEBUG=+seh,+err; fi
 Xvfb :0 -screen 0 1024x768x16 &
 XVFB_PID=$!
@@ -7,4 +9,4 @@ trap 'kill "$XVFB_PID" 2>/dev/null || true; wait "$XVFB_PID" 2>/dev/null || true
 export DISPLAY=:0
 wineboot -u
 wineserver -w
-wine /cad2data/DDC_CONVERTER_Revit2IFC/RVT2IFCconverter.exe /input/in.rvt /output/out.ifc
+wine /cad2data/DDC_CONVERTER_Revit2IFC/RVT2IFCconverter.exe "$IN_FILE" "$OUT_FILE"
