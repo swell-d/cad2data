5975e82527ba97954c061d798061746754f74c7f
Dmitry Rude <3952800@gmail.com>
2025-10-17 12:42:30 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,5 +10,5 @@ mkdir -p "$WINEPREFIX" "$XDG_RUNTIME_DIR"
 if [[ -d "$BASE_PREFIX" && -f "$BASE_PREFIX/system.reg" ]]; then rsync -a --delete "$BASE_PREFIX"/ "$WINEPREFIX"/; fi
 wineboot -u
 trap 'wineserver -k || true; rm -rf "$WINEPREFIX"' EXIT INT TERM
-xvfb-run -a -s "-screen 0 1024x768x16 -nolisten tcp" wine64 /cad2data/DDC_CONVERTER_Revit2IFC/DDC_REVIT2IFC_CONVERTER/RVT2IFCconverter.exe "$1" "$2"
+xvfb-run -a -s "-screen 0 1024x768x16 -nolisten tcp" wine /cad2data/DDC_CONVERTER_Revit2IFC/DDC_REVIT2IFC_CONVERTER/RVT2IFCconverter.exe "$1" "$2"
 wineserver -w
