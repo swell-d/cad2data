cadcab335356d7571d0c7ca429edefbd6f151728
Dmitry Rude <3952800@gmail.com>
2025-10-17 13:29:41 +0200

    Enhance entrypoint.sh to set up Wine environment
    Added commands to create necessary directories for Wine and start the Gunicorn server.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 set -e
 
 echo "Starting..."
+mkdir -p "$WINEPREFIX"
+mkdir -p "$XDG_RUNTIME_DIR"
+xvfb-run -a -s "-screen 0 1024x768x16 -nolisten tcp" wineboot -u && wineserver -w
+echo "Wine loaded. Starting gunicorn..."
 exec micromamba run -n myenv gunicorn --workers=1 --bind 0.0.0.0:5001 server:app
